6e66fb9a87667032d060c2f895bc69439e7b8d24
Bryan Bibat <bry@bryanbibat.net>
2012-12-17 21:04:17 +0800

    Fix Utils indention

--- lib/smart_devnet/utils.rb
+++ lib/smart_devnet/utils.rb
@@ -4,26 +4,26 @@ module SmartDevnet
   class Utils
     class << self
 
-    def process_error(response)
-      if response.body.include? "requestError"
-        error_type = response.body.include?("serviceException") ?
-          "serviceException" : "policyException"
-        exception = JSON.parse(response.body)["requestError"][error_type]
-        return "#{exception["messageId"]} #{interpolate(exception["text"], exception["variables"])}"
-      else
-        return "#{response.code} #{response.message}"
+      def process_error(response)
+        if response.body.include? "requestError"
+          error_type = response.body.include?("serviceException") ?
+            "serviceException" : "policyException"
+          exception = JSON.parse(response.body)["requestError"][error_type]
+          return "#{exception["messageId"]} #{interpolate(exception["text"], exception["variables"])}"
+        else
+          return "#{response.code} #{response.message}"
+        end
       end
-    end
 
-    def interpolate(text, variables)
-      variables = [variables] unless variables.is_a? Array
-      str = ""
-      while text =~ /%(\d+)/
-        str += "#{$`}#{variables[$1.to_i - 1]}"
-        text = $'
+      def interpolate(text, variables)
+        variables = [variables] unless variables.is_a? Array
+        str = ""
+        while text =~ /%(\d+)/
+          str += "#{$`}#{variables[$1.to_i - 1]}"
+          text = $'
+        end
+        str += text
       end
-      str += text
-    end
 
     end
   end
